4ebccc9583d5fb578bdda11afd0e3899852b1be1
Redwine708 <1802407593@qq.com>
2021-06-13 10:57:54 +0800

    add trap problem

---
+++ DP/trap/trap.cpp
@@ -0,0 +1,43 @@
+//LeetCode : dynamic programing 
+#include<vector>
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int res = 0;
+        for(int i = 2 ; i < height.size() ; i ++)
+        {
+            if(height[i] <= height[i-1])continue;
+            else{
+                //find the maximum height of all height[0...i-1]
+                int max = i-2 ;
+                for(int j = 0 ; j < i-2 ; j ++)
+                {
+                    if(height[j] > height[max]) max = j;
+                }
+                int secMax = height[i];
+                if(height[max] < secMax) secMax = height[max];
+                for(int m = max + 1 ; m < i ; m ++)
+                {
+                    if(height[m] < secMax){
+                    res += secMax - height[m];
+                    height[m] = secMax;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    vector<int>height(12);
+    for(int i = 0 ;  i< 12 ; i ++)
+    {
+        cin>>height[i];
+    }
+    Solution s;
+    cout<<s.trap(height);
+}
